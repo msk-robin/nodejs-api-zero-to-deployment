1e98ed15d64f727ad6b880a3b0dcbafd6d632db8
lostLegion <robikanambo@gmail.com>
2026-03-06 07:17:03 +0300

    chore: fixed lint syntax exit code 1

--- utils.js
+++ utils.js
@@ -1,35 +1,35 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
-export function checkPasswordStrength(password) {
+export function checkPasswordStrength (password) {
   // Minimum eight characters, at least one letter, one number and one special character:
   const strengthRegex =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-  return strengthRegex.test(password);
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+  return strengthRegex.test(password)
 }
 
-//edited data
-export function generateToken(data) {
+// edited data
+export function generateToken (data) {
   return jwt.sign(
     {
-      data: data,
+      data
     },
     process.env.JWT_SECRET,
-    { expiresIn: "1h" },
-  );
+    { expiresIn: '1h' }
+  )
 }
 
-export function requireAuthentication(req, res, next) {
-  const token = req.headers.authentication;
+export function requireAuthentication (req, res, next) {
+  const token = req.headers.authentication
   if (!token) {
-    res.status(401).json({ error: "No token provided" });
-    return;
+    res.status(401).json({ error: 'No token provided' })
+    return
   }
   try {
-    const accessToken = token.split(" ")[1];
-    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
-    req.user = decoded.data;
-    next();
+    const accessToken = token.split(' ')[1]
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET)
+    req.user = decoded.data
+    next()
   } catch (err) {
-    res.status(401).json({ error: "Invalid token" });
+    res.status(401).json({ error: 'Invalid token' })
   }
 }
