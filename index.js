8c84d17cf1bb90293c848849c344704bcbdbd68d
lostLegion <robikanambo@gmail.com>
2026-03-06 15:58:53 +0300

    chore: fixed lint exit code 1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import { app } from "./server.js";
-import mongoose from "mongoose";
+import { app } from './server.js'
+import mongoose from 'mongoose'
 
-//use railway provide port and fallback 3000 for local dev
-const port = process.env.PORT || 3000;
+// use railway provide port and fallback 3000 for local dev
+const port = process.env.PORT || 3000
 
 try {
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log("Connected to MongoDB");
+  await mongoose.connect(process.env.MONGODB_URI)
+  console.log('Connected to MongoDB')
 
   app.listen(port, () => {
-    //for logs, show actual railway url if available, otherwise show localhost
+    // for logs, show actual railway url if available, otherwise show localhost
     const publicURL =
-      process.env.RAILWAY_STATIC_URL || `http://localhost:${port}`;
-    console.log(`APp running at ${publicURL}`);
-  });
+      process.env.RAILWAY_STATIC_URL || `http://localhost:${port}`
+    console.log(`APp running at ${publicURL}`)
+  })
 } catch (error) {
-  console.error(error);
+  console.error('Failed to start server', error)
 }
